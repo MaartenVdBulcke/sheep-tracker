1e5a1f266050bb34666e2810529b88b6e8417f39
maartenvdbulcke-intelliprove <maarten.vandenbulcke@intelliprove.be>
2022-03-20 00:47:59 +0100

    add csv functionality

--- src/track_sheep.py
+++ src/track_sheep.py
@@ -1,6 +1,9 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import urllib.request
+import csv
+import os
+
 
 url_export = 'https://data.stad.gent/explore/dataset/sheep-tracking-gent/export/'
 
@@ -17,17 +20,26 @@ print(f'geo {geo}')
 driver.quit()
 
 # extract values from geojson
-print(type(geo))
-print(geo['features'])
-print(geo['features'][0]['geometry']['coordinates'])
+# print(type(geo))
+# print(geo['features'])
+# print(geo['features'][0]['geometry']['coordinates'])
 data = eval((geo['features'][0]['properties']['data']))
-print(data['name'])
-print(data['time'])
-print(data['source'])
-print(data['state'])
-print(data['address'])
+# print(data['name'])
+# print(data['time'])
+# print(data['source'])
+# print(data['state'])
+# print(data['address'])
 print(geo['features'][0]['properties']['lat'])
 print(geo['features'][0]['properties']['lng'])
 
 
 # collect values in csv file 
+csv_file_path = r'..\csv\sheep_whereabouts.csv'
+write_header = not os.path.isfile(csv_file_path)
+with open(csv_file_path, 'a', newline='') as sw:
+    writer_object = csv.writer(sw)
+    if write_header:
+        writer_object.writerow(["time", "latitude", "longitude"])
+    writer_object.writerow([data['time'], geo['features'][0]['properties']['lat'], geo['features'][0]['properties']['lng']])
+    sw.close()
+    
