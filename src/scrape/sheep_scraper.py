a003985c671b1f5c92919ae77ab0e01147ccdb3d
maartenvdbulcke-intelliprove <maarten.vandenbulcke@intelliprove.be>
2022-03-29 22:39:45 +0200

    lose redundant helper function

--- src/scrape/sheep_scraper.py
+++ src/scrape/sheep_scraper.py
@@ -28,28 +28,21 @@ class SheepScraper:
         self.geojson: dict = eval((geo['features'][0]['properties']['data']))
 
     def append_to_the_herd_history(self):
+        self.nowtime: str = time.strftime('%Y %b %d, %H:%M:%S', time.localtime())
+
         csv_file_path = SheepScraper.__fix_path()
         write_header = not os.path.isfile(csv_file_path)
-
         with open(csv_file_path, 'a', newline='') as sheep_history:
             writer_object = csv.writer(sheep_history)
             if write_header:
-                writer_object.writerow(
-                    [
+                writer_object.writerow([
                         "scrape time", "time", "latitude", "longitude", 
                         "name", "source", "state", "address"
-                    ]
-                )
-            
-            SheepScraper.__freeze_now_time(self)
-
-            writer_object.writerow(
-                [
+                ])            
+            writer_object.writerow([
                     self.nowtime, self.geojson['time'], self.geojson['lat'], self.geojson['lng'],
                     self.geojson['name'], self.geojson['source'], self.geojson['state'], self.geojson['address']
-                ]
-            )
-            
+            ])
             sheep_history.close()
     
     @staticmethod
@@ -58,8 +51,3 @@ class SheepScraper:
         if not os.path.isdir(SheepScraper.FOLDER_PATH):
             os.mkdir(SheepScraper.FOLDER_PATH)
         return os.path.join(SheepScraper.FOLDER_PATH, SheepScraper.FILENAME)
-
-    def __freeze_now_time(self):
-        t = time.localtime()
-        self.nowtime: str = time.strftime('%Y %b %d, %H:%M:%S', t)
-    
