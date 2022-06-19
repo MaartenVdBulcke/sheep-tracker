ff2a44a9122844676e763a39678fa00743c4d630
maartenvdbulcke-intelliprove <maarten.vandenbulcke@intelliprove.be>
2022-06-19 15:29:15 +0200

    refactor to include Driver class

--- src/find_them.py
+++ src/find_them.py
@@ -3,5 +3,6 @@ from scrape import SheepScraper
 
 if __name__=='__main__':
     find_them = SheepScraper()
-    find_them.scrape_geojson()
+    find_them.scrape_geojson_data()
     find_them.append_to_the_herd_history()
+    find_them.quit_selenium_driver()
--- src/scrape/sheep_scraper.py
+++ src/scrape/sheep_scraper.py
@@ -1,53 +1,61 @@
-from typing import Optional
-from selenium import webdriver
-from selenium.webdriver.common.by import By
 import urllib.request
 import csv
 import os
 import time 
+from typing import Optional
+
+from .driver import Driver
 
 
 class SheepScraper:
+    """
+    Class that interacts dynamically with the sheep whereabouts dataset 
+    and stores the data in a csv-file.
+    """
     
-    URL_EXPORT = r'https://data.stad.gent/explore/dataset/sheep-tracking-gent/export/'
+    SHEEP_DATA_URL = r'https://data.stad.gent/explore/dataset/sheep-tracking-gent/export/'
     CWD = r'C:\Users\maart\Documents\myProjects\sheep'
     FOLDER_PATH = r'csv'
     FILENAME = r'sheep_whereabouts.csv'
 
     def __init__(self):
-        pass
+        self.driver: Driver = Driver(SheepScraper.SHEEP_DATA_URL)
+        self.geodata: Optional[dict] = None
 
-    def scrape_geojson(self):
-        driver = webdriver.Chrome()
-        driver.get(SheepScraper.URL_EXPORT)
-        geojson_button = driver.find_element(By.XPATH, '//div[@format-extension="geojson"]/a')
-        href = geojson_button.get_attribute('href')
-        file = urllib.request.urlopen(href)
-        geo = eval(file.read().decode('utf-8'))
-        driver.quit()
-        self.geojson: dict = eval((geo['features'][0]['properties']['data']))
+    def scrape_geojson_data(self):
+        geojson_webelement = self.driver.get_geojson_webelement()
+        href_url = geojson_webelement.get_attribute('href')
+        dynamic_file = urllib.request.urlopen(href_url)
+        geo_info = eval(dynamic_file.read().decode('utf-8'))
+        self.geodata = eval((geo_info['features'][0]['properties']['data']))
 
     def append_to_the_herd_history(self):
-        self.nowtime: str = time.strftime('%Y %b %d, %H:%M:%S', time.localtime())
-
-        csv_file_path = SheepScraper.__fix_path()
+        csv_file_path = SheepScraper._fix_path()
         write_header = not os.path.isfile(csv_file_path)
         with open(csv_file_path, 'a', newline='') as sheep_history:
             writer_object = csv.writer(sheep_history)
             if write_header:
                 writer_object.writerow([
-                        "scrape time", "time", "latitude", "longitude", 
-                        "name", "source", "state", "address"
+                    "scrape time", "time", "latitude", "longitude", 
+                    "name", "source", "state", "address"
                 ])            
             writer_object.writerow([
-                    self.nowtime, self.geojson['time'], self.geojson['lat'], self.geojson['lng'],
-                    self.geojson['name'], self.geojson['source'], self.geojson['state'], self.geojson['address']
+                SheepScraper._get_time(), self.geodata['time'], self.geodata['lat'], self.geodata['lng'],
+                self.geodata['name'], self.geodata['source'], self.geodata['state'], self.geodata['address']
             ])
             sheep_history.close()
     
+    def quit_selenium_driver(self):
+        self.driver.quit()
+    
     @staticmethod
-    def __fix_path():
+    def _fix_path():
+        """Assures working directory is stable (also when script is run from batch file)"""
         os.chdir(SheepScraper.CWD)
         if not os.path.isdir(SheepScraper.FOLDER_PATH):
             os.mkdir(SheepScraper.FOLDER_PATH)
         return os.path.join(SheepScraper.FOLDER_PATH, SheepScraper.FILENAME)
+    
+    @staticmethod
+    def _get_time() -> str:
+        return time.strftime('%Y %b %d, %H:%M:%S', time.localtime())
